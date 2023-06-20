0db94f2ee8899b60eca8626a1a1ee2caf150b386
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-06-21 00:27:24 +0900

    :construction: 임시로 api키 상수처리
    임시로 api키 상수처리

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -5,3 +5,5 @@ export type THEME_MODE = (typeof THEME)[number];
 
 export const TYPE = ["popular", "now_playing", "upcoming"] as const;
 export type TYPE_MODE = (typeof TYPE)[number];
+
+export const VITE_API_KEY = "ffb94059c259041348b0799147e0ade9";
